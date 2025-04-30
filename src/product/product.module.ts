39cb9bc14cf3fa7274d33b0bab36e629714a732b
syedmahmad <devm.ahmad@gmail.com>
2025-04-30 12:07:58 +0500

    hot fix

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -6,12 +6,12 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { ProductImage } from 'src/database/entity/product.images.entity';
 import { CampaignProduct } from 'src/database/entity/campaign.product.entity';
 import { SalesCampaign } from 'src/database/entity/sales.campaign.entity';
-import { productProvider } from 'src/product/providers/product.provider';
-import { productImageProvider } from 'src/product/providers/productImage.provider';
-import { storeProvider } from 'src/product/providers/store.provider';
-import { salesCampaignProvider } from 'src/product/providers/saleCampaign.provider';
-import { campaignProductProvider } from 'src/product/providers/campaign.product.provider';
-import { merchantProvider } from 'src/product/providers/merchant.provider';
+import { productImageProvider } from 'src/product/Providers/productImage.provider';
+import { storeProvider } from 'src/product/Providers/store.provider';
+import { salesCampaignProvider } from 'src/product/Providers/saleCampaign.provider';
+import { campaignProductProvider } from 'src/product/Providers/campaign.product.provider';
+import { merchantProvider } from 'src/product/Providers/merchant.provider';
+import { productProvider } from 'src/product/Providers/product.provider';
 
 
 
