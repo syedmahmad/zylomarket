bc8dfbc835949255a9d72d047b3f0d7cf30fe32b
syedmahmad <devm.ahmad@gmail.com>
2025-04-30 10:32:39 +0500

    allowed frontend url

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule); // ✅ use Express app
 
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: ['http://localhost:3001', 'https://ecommerce-store-six-ruddy.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
